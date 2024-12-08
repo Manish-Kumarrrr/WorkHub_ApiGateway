dff8a301bc53a299c7429313678eed1bb3ac9a69
Manish Kumar <mkc1020304050@gmail.com>
2024-12-08 13:31:29 +0530

    spring cloud gateway integerated

---
+++ src/main/java/com/manish/WorkHub/WorkHubApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.manish.WorkHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkHubApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkHubApiGatewayApplication.class, args);
+	}
+
+}
